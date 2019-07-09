3e67e716a1bc0085d340603110b95e50946d5474
percoscode <50783968+percoscode@users.noreply.github.com>
2019-07-08 21:58:39 -0700

    added basic async await bones

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import './App.css';
 
+const urls = [
+  'https://swapi.co/api/vehicles'
+]
+
+const getData = async function() {
+  try {
+    const [ name, model, manufacturer ] = await Promise.all(urls.map(url =>
+      fetch(url, {mode: 'no-cors'}).then(resp => resp.json())
+      ))
+      console.log('name', name)
+      console.log('model', model)
+      console.log('manufacturer', manufacturer)
+  } catch (err) {
+    console.log('oops', err)
+  };
+}
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  componentDidMount() {
+    console.log('working')
+    getData()
+  }
+
   render() {
     return (
     <h1>Is this working, now?</h1>
     )
-  }
-  componentDidMount() {
-    console.log('working')
   }
 }
 
