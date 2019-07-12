7fb4e43e3a680a80c9a59133732554e9daba85e7
percoscode <50783968+percoscode@users.noreply.github.com>
2019-07-11 21:06:01 -0700

    proper async functionality in the works

--- src/App.js
+++ src/App.js
@@ -2,20 +2,40 @@ import React from 'react';
 import './App.css';
 
 const urls = [
-  'https://swapi.co/api/vehicles'
+	// "https://jsonplaceholder.typicode.com/users",
+	// "https://jsonplaceholder.typicode.com/posts",
+  // "https://jsonplaceholder.typicode.com/albums",
+  "https://swapi.co/api/vehicles/?format=json"
 ]
 
-const getData = async function() {
+// example from ZTM
+// const getData1 = async function() {
+// 	try {
+// 		const [ users, posts, albums ] = await Promise.all(urls.map(url => 
+// 			fetch(url).then(resp => resp.json())
+// 		))
+// 		console.log('users', users)
+// 		console.log('posts', posts)
+// 		console.log('albums', albums)
+// 	} catch (err) {
+// 		console.log('oofda', err)
+// 	}
+// }
+
+// hits api, but logs vars as undefined
+const getData = async () => {
   try {
     const [ name, model, manufacturer ] = await Promise.all(urls.map(url =>
-      fetch(url, {mode: 'no-cors'}).then(resp => resp.json())
+      fetch(url).then(resp => resp.json())
       ))
-      console.log('name', name)
-      console.log('model', model)
-      console.log('manufacturer', manufacturer)
+    console.log('name', name)
+    console.log('model', model)
+    console.log('manufacturer', manufacturer)
   } catch (err) {
-    console.log('oops', err)
-  };
+    console.log('oofda', err)
+  } finally {
+    console.log('nobody died')
+  }
 }
 
 class App extends React.Component {
